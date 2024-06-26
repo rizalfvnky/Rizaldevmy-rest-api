94894f22d6b13e7a0eff43b7f468fb44ab9c732a
Rizaldev <168670936+rizalfvnky@users.noreply.github.com>
2024-06-27 05:48:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Rizaldev-api'// yourname
+global.MONGO_DB_URI = "mongodb+srv://rizaldev:rizaldev123@rizaldev.ory3qij.mongodb.net/?retryWrites=true&w=majority&appName=rizaldev" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rizaldev00@gmail.com" //email
+global.email_password = "fzml psvh rref hcmu" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
